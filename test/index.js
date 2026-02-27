9604f02611266a73a5b48413148a83e08763eb2e
NaughtySora <naughtysora@proton.me>
2026-02-27 22:30:00 +0200

    simple tests for enter and leave

--- test/index.js
+++ test/index.js
@@ -5,48 +5,62 @@ const { describe, it } = require('node:test');
 const { misc, async } = require("naughty-util");
 const Semaphore = require('../lib/Semaphore');
 
+const CONCURRENCY = 2;
+const TEST_COUNT = 10000;
+const RACE_CONDITION_MESSAGE = 'race condition';
+
 const raceCondition = async (min, max) => {
   raceCondition.count++;
-  await async.pause(misc.random(max, min));
-  if (raceCondition.count > COUNT) {
-    throw new Error('race condition');
+  const delay = (min + max) === 0 ? 0 : misc.random(max, min);
+  await async.pause(delay);
+  if (raceCondition.count > CONCURRENCY) {
+    throw new Error(RACE_CONDITION_MESSAGE);
   }
   raceCondition.count--;
 };
 Object.assign(raceCondition, { count: 0 });
 
-const CONCURRENCY = 2;
-
 describe('Semaphore', async () => {
-  const lock = new Semaphore(CONCURRENCY);
 
   await it('enter - leave', async () => {
-    await lock.enter();
-    await raceCondition(0, 0);
-    lock.leave();
+    const lock = new Semaphore(CONCURRENCY);
+    const test = async () => {
+      await lock.enter();
+      await raceCondition(0, 0);
+      lock.leave();
+    };
+    await Promise.all(Array.from({ length: TEST_COUNT }, test));
   });
 
   await it('enter - leave - signal', async () => {
-    const errors = [];
-    try {
-      await lock.enter(AbortSignal.timeout(misc.random(5000, 100)));
-      await raceCondition(0, 0);
-      lock.leave();
-    } catch (e) { }
+    let errors = 0;
+    const lock = new Semaphore(CONCURRENCY);
+    const test = async () => {
+      const delay = misc.random(5000, 100);
+      try {
+        await lock.enter(AbortSignal.timeout(delay));
+        await raceCondition(0, 0);
+        lock.leave();
+      } catch (e) {
+        if (e.message === RACE_CONDITION_MESSAGE) errors++;
+      }
+    };
+    await Promise.all(Array.from({ length: TEST_COUNT }, test));
+    assert.equal(errors, 0);
   });
 
-  await it('isolate', async () => {
-    await lock.isolate(raceCondition, { args: [0, 0] });
-  });
+  // await it('isolate', async () => {
+  //   await lock.isolate(raceCondition, { args: [0, 0] });
+  // });
 
-  await it('isolate - signal', async () => {
-    const errors = [];
-    try {
-      await lock.isolate(raceCondition, {
-        args: [0, 0],
-        signal: AbortSignal.timeout(misc.random(5000, 100))
-      });
-    } catch (e) { }
-  });
+  // await it('isolate - signal', async () => {
+  //   const errors = [];
+  //   try {
+  //     await lock.isolate(raceCondition, {
+  //       args: [0, 0],
+  //       signal: AbortSignal.timeout(misc.random(5000, 100))
+  //     });
+  //   } catch (e) { }
+  // });
 });
 
